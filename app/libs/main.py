dcb04666154a7ca40951f0fea1f58104bdaa0a36
Timur <timursamigulin98@gmail.com>
2022-09-08 15:46:19 +0300

    update gitignore

--- app/libs/main.py
+++ app/libs/main.py
@@ -3,7 +3,7 @@ from app.schemas.messages import MessageBase, MessageWithEmotions
 
 
 async def psycho_text_analyze(message: MessageBase):
-    """Основная функция, в которой сообщения проходит весь пайплайн от предобработки и вычесления метрик до записи в базу и отправки пользователю
+    """Основная функция, в которой сообщения проходит весь пайплайн предобработки и вычесления метрик 
 
     Args:
         message (MessageBase): сообщение 
@@ -14,9 +14,8 @@ async def psycho_text_analyze(message: MessageBase):
     # Анализируем все предложение
     message_with_emotion = await message_get_psycho_metrics(message)
     # Находим итоговую эмоцию для сообщения
-    # print(sents_emotion_list)
-    # print(message_with_emotion)
     message_emotion = await calculate_coef(message_with_emotion, sents_emotion_list)
+    # Получаем совет
     advices = await get_advice(message, message_emotion)
 
     message_with_emotion = MessageWithEmotions(
@@ -24,6 +23,5 @@ async def psycho_text_analyze(message: MessageBase):
         emotion=message_emotion,
         advice=advices
     )
-    # await save_all_sents(message_with_emotion)
 
     return message_with_emotion
